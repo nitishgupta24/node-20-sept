f82443c030feabec691286b9bb57aa19a797ed60
nitishgupta24 <guday278@gmail.com>
2024-09-23 15:38:38 +0530

    third project

---
+++ controllers/studentcontroller.js
@@ -0,0 +1,26 @@
+const Student = require('../models/Student');
+
+async function addStudent(req, res) {
+     try {
+         console.log(req.body);
+         let student = new Student(req.body);
+         await student.save();
+         res.render("studentInsert")
+     } catch (error) {
+         console.log(error);
+     }
+}
+async function getStudents(req,res) {
+    try {
+        let students = await Student.find({});
+        res.render('studentlist', {
+            students: students
+        });
+    } catch (err) {
+        console.log(err)
+    }
+}
+module.exports = {
+    addStudent,
+    getStudents
+}
