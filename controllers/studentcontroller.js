7c2d60d0bd15c9b6009b8c7c689f43bcdeb4d881
nitishgupta24 <guday278@gmail.com>
2024-09-24 15:32:38 +0530

    updation

--- controllers/studentcontroller.js
+++ controllers/studentcontroller.js
@@ -20,7 +20,43 @@ async function getStudents(req,res) {
         console.log(err)
     }
 }
+async function getStudentForEdit(req, res) {
+    try {
+        let id = req.params.id;
+        console.log(id);
+        let student = await Student.findOne({ _id: id });
+          console.log("Student: " + student);
+    // res.send(student);
+    res.render("studentforupdate", { student: student });
+  } catch (err) {
+    console.error("Something went wrong!!", err);
+  }
+}
+async function updateStudent(req, res) {
+    try {
+        let id = req.params.id;
+        let student = await Student.findOne({ _id: id });
+        console.log(student, 'student');
+        student.rollNo = req.body.rollNo;
+        student.firstName = req.body.firstName;
+        student.lastName = req.body.lastName;
+        student.fatherName = req.body.fatherName;
+        student.adharCardNo = req.body.adharCardNo;
+        student.mobileNo = req.body.mobileNo;
+        await student.save();
+        let students = await Student.find({});
+        res.render('studentlist', {
+            students: students
+        })
+        
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
 module.exports = {
     addStudent,
-    getStudents
+    getStudents,
+    getStudentForEdit,
+    updateStudent
 }
--- routes/student.js
+++ routes/student.js
@@ -12,4 +12,13 @@ router.post('/add/student', (req, res) => {
 router.get('/students', (req, res) => {
     studentcontroller.getStudents(req,res)
 })
+router.get('/edit/student/page/:id', (req, res) => {
+    studentcontroller.getStudentForEdit(req,res)
+})
+router.post('/update/student/:id', (req, res) => {
+    studentcontroller.updateStudent(req,res)
+})
+router.get('/delete/student/:id', (req, res) => {
+    studentcontroller.deleteStudent(req,res)
+})
 module.exports = router;
